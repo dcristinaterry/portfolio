074855d2f6999c3f4f1736c295ef5576f4b639b4
dcristinaterry <crisdc88@gmail.com>
2020-07-07 11:46:39 -0500

    Updates

---
+++ precache-manifest.f5074015f0730366ee12c93967e0ce6c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a2df898a65daaae40c581441143306",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b63710f95495f7c4a7d",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "445408f960c924c37c52",
+    "url": "/static/css/main.0d989b3a.chunk.css"
+  },
+  {
+    "revision": "6b63710f95495f7c4a7d",
+    "url": "/static/js/2.f31813cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f31813cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "445408f960c924c37c52",
+    "url": "/static/js/main.a532fa7a.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
