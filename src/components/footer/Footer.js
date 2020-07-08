676f3ccc9a678a9b1b92b723b1417b2140644da2
dcristinaterry <crisdc88@gmail.com>
2020-07-07 19:41:02 -0500

    adjust font size footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
 
         <footer class="bg-gray-300 py-6">
             <div class="text-center ">
-                <h3 class="text-gray-920 text-xl lg:text-2xl font-spaceMono font-light">Built with React and Tailwind CSS.</h3>
+                <h3 class="text-gray-920 text-lg lg:text-xl font-spaceMono font-light">Built with React and Tailwind CSS.</h3>
             </div>
         </footer>
     )
