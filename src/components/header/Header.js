73aacb5b496be5d2b26b7737a88126b559fa84d2
dcristinaterry <crisdc88@gmail.com>
2020-07-07 20:44:40 -0500

    adjust pb to 20 for sm screen and mobile

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
 
 
     return (
-        <header className="bg-gray-910 relative pb-2 sm:pb-24 md:pt-20 md:pb-40">
+        <header className="bg-gray-910 relative pb-20 sm:pb-24 md:pt-20 md:pb-40">
             <div className="text-center">
                 <h1 className="text-5xl sm:text-4xl text-white-101 font-megrim font-black  md:text-6xl  lg:text-9xl pt-6 ">Cristina Terry</h1>
                 <h3 className="text-xs sm:text-base  md:text-lg lg:text-1xl text-white-101 font-syncopate sm:mb-4" >Full-Stack Web Developer / Software Engineer</h3>
