2852e9c69415900d7e27ff62615c418f6bf00952
dcristinaterry <crisdc88@gmail.com>
2020-07-07 19:52:54 -0500

    adjust font size header

--- tailwind.config.js
+++ tailwind.config.js
@@ -276,6 +276,7 @@ module.exports = {
       base: '1rem',
       lg: '1.125rem',
       xl: '1.25rem',
+      '1xl': '1rem',
       '2xl': '1.5rem',
       '3xl': '1.875rem',
       '4xl': '2.25rem',
