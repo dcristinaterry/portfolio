497ad774fd7e10b788d74b165d80ed5b47e4ce95
dcristinaterry <crisdc88@gmail.com>
2020-07-07 19:39:54 -0500

    adjust font size header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <header className="bg-gray-910 relative pb-2 sm:pb-24 md:pt-20 md:pb-40">
             <div className="text-center">
-                <h1 className="text-5xl sm:text-6xl text-white-101 font-megrim font-black  md:text-6xl  lg:text-10xl pt-6 ">Cristina Terry</h1>
+                <h1 className="text-5xl sm:text-4xl text-white-101 font-megrim font-black  md:text-4xl  lg:text-6xl pt-6 ">Cristina Terry</h1>
                 <h3 className="text-xs sm:text-base  md:text-lg lg:text-2xl text-white-101 font-syncopate sm:mb-4" >Full-Stack Web Developer / Software Engineer</h3>
             </div>
             <div className=" mt-8 text-right bottom-0 right-0 pr-4 md:pr-4 absolute">
